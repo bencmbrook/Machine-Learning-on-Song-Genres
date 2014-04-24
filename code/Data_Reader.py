b5d5dc2d8b09c512ce4e4638f8600f1631f2175d
Benjamin Brook <benjaminbrook@college.harvard.edu>
2014-04-24 00:44:16 -0400

    API coming along

---
+++ code/Data_Reader.py
@@ -0,0 +1,16 @@
+from pyechonest import config
+config.ECHO_NEST_API_KEY="SRCGPBCAPG5FQQKFR"
+
+import json
+
+from pyechonest import artist
+bk = artist.Artist('bikini kill')
+print "Artists similar to: %s:" % (bk.name,)
+for similar_artist in bk.similar: print "\t%s" % (similar_artist.name,)
+
+
+
+genres = []
+
+
+song = (genre, {'danceability': danceability, 'energy': energy, 'tempo': tempo, 'loudness': loudness, 'time_signature': time_signature, 'duration': duration, 'mode': mode})
--- code/EchoNest_Handler.py
+++
@@ -1,2 +0,0 @@
-
-song = (genre, {'danceability': danceability, 'energy': energy, 'tempo': tempo, 'loudness': loudness, 'time_signature': time_signature, 'duration': duration, 'mode': mode})
