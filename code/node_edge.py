c0672833139c05797c0e8522b9c669d4c6162207
Jake Lynch <jacoblynch@college.harvard.edu>
2014-04-28 22:23:55 -0400

    1023

--- code/node_edge.py
+++ code/node_edge.py
@@ -1,53 +1,36 @@
-from Data_Reader import get_songs
-songs = get_songs()
-
-class Node
+class Node:
     def __init__(self):
         self.EdgesIn = []
         self.EdgesOut = []
         self.LastOutputs = []
-        self.LastInput = []
-        self.Error = None
+        self.Error = 0.0
+
          
     def Evaluate(self, input):
+        sum = 0
         
-        #store the outputs of each edge in an array
-        for i, e in enumerate(self.EdgesIn):
-            self.LastInput[i] = (e.in.Evaluate(input) * e.weight)
-        
-        #sum the elements of the input array
-        output = sum(self.LastInput)
-        
-        #store the value to be outputted in the output array
-        self.LastOutputs.append(output)    
+        #sum the outputs of each incoming edge
+        for e in self.EdgesIn
+            sum += sum + (e.in.Evaluate(input) * e.weight)
+         
+        self.LastOutputs.append(sum)    
         return sum
+
+    def evaluate(self, inputVector):
+      self.lastInput = []
+      weightedSum = 0
+ 
+      for e in self.incomingEdges:
+         theInput = e.source.evaluate(inputVector)
+         self.lastInput.append(theInput)
+         weightedSum += e.weight * theInput
+ 
+      self.lastOutput = activationFunction(weightedSum)
+      return self.lastOutput
     
-    def EvalError(self, truth):
-        
-        #if for some reason we haven't learned from our last error
-        if self.Error != None
-            return self.Error
-        
-        #if current node is an output node calculate error and return it
-        else if self.EdgesOut == []
-            self.Error = truth - self.LastOutputs[0]
-            return self.Error
-        
-        #else sum errors of output edges and eventually return error
-        else
-            for e in self.EdgesOut:
-                self.Error += (e.weight * e.out.EvalError(truth))
-                
-            return self.Error
-        
-    def Learn(self, LearnRate):
-        if not_(self.LastOutputs = [] or self.Error = None or self.LastInput = None)
-            for i, e in enumerate(self.EdgesIn):
-                e.weight += (LearnRate * self.LastOutputs[0] * (1 - self.LastOutputs[0]) *
-                             self.Error * self.LastInput[i]
+
+       
             
-            self.Error = None
-            self.LastInput = None
         
 class Input_Node(Node): 
     def __init__(self, index):
@@ -55,15 +38,15 @@ class Input_Node(Node):
         
         #add index of the input to be assigned to this node
         self.index = index; 
-        
-    def Evaluate(self, input):
-        #should just return the correct value identified by index from the input
-        output = input[self.index]
-        self.LastOutputs.append(output)
-        return output
+
+   
+    def evaluate(self, inputVector):
+      self.lastOutput = inputVector[self.index]
+      return self.lastOutput
+
 
 class Edge
-    def __init__(self, in, out)
+    def __init__(self, in, out):
         self.weight = random.uniform(0,1)
         self.in = in
         self.out = out
@@ -72,5 +55,36 @@ class Edge
         in.EdgesOut.append(self)
         out.EdgesIn.append(self)
 
+class BiasNode(InputNode):
+   def __init__(self):
+      Node.__init__(self)
+ 
+   def evaluate(self, inputVector):
+      return 1.0
+
+class NeuralNetwork
+    def __init__(self):
+      self.inputNodes = []
+      self.outputNode = None
+
+    def evaluate(self, inputVector):
+      return self.outputNode.evaluate(inputVector)
+
+    def propagateError(self, label):
+      for node in self.inputNodes:
+         node.getError(label)
+ 
+   def updateWeights(self, learningRate):
+      for node in self.inputNodes:
+         node.updateWeights(learningRate)
+ 
+   def train(self, labeledExamples, learningRate=0.9, maxIterations=10000):
+      while maxIterations > 0:
+         for example, label in labeledExamples:
+            output = self.evaluate(example)
+            self.propagateError(label)
+            self.updateWeights(learningRate)
+ 
+            maxIterations -= 1
         
         
