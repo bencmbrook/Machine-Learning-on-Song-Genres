5516cf0cec5212508a68b615c51e1a05aa98924a
Benjamin Brook <benjaminbrook@college.harvard.edu>
2014-04-29 00:27:51 -0400

    network, main temp updated

--- code/main_temp.py
+++ code/main_temp.py
@@ -1,21 +1,23 @@
+import network
+
 def binaryNumbersTest():
-   network = Network()
-   inputNodes = [Input_Node(i) for i in range(3)]
-   hiddenNodes = [Node() for i in range(3)]
-   outputNode = Node()
+    network = Network()
+    inputNodes = [Input_Node(i) for i in range(3)]
+    hiddenNodes = [Node() for i in range(3)]
+    outputNode = Node()
 
-   # weights are all randomized
-   for inputNode in inputNodes:
-      for node in hiddenNodes:
-         Edge(inputNode, node)
+    # weights are all randomized
+    for inputNode in inputNodes:
+        for node in hiddenNodes:
+           Edge(inputNode, node)
 
-   for node in hiddenNodes:
-      Edge(node, outputNode)
+    for node in hiddenNodes:
+        Edge(node, outputNode)
 
-   network.outputNode = outputNode
-   network.inputNodes.extend(inputNodes)
+    network.outputNode = outputNode
+    network.inputNodes.extend(inputNodes)
 
-   labeledExamples = [((0,0,0), 1),
+    labeledExamples = [((0,0,0), 1),
                       ((0,0,1), 0),
                       ((0,1,0), 1),
                       ((0,1,1), 0),
@@ -23,8 +25,8 @@ def binaryNumbersTest():
                       ((1,0,1), 0),
                       ((1,1,0), 1),
                       ((1,1,1), 0)]
-   network.train(labeledExamples, maxIterations=5000)
+    network.train(labeledExamples, maxIterations=5000)
 
-   # test for consistency
-   for number, isEven in labeledExamples:
-      print "Error for %r is %0.4f. Output was:%0.4f" % (number, isEven - network.evaluate(number), network.evaluate(number))
+    # test for consistency
+    for number, isEven in labeledExamples:
+        print "Error for %r is %0.4f. Output was:%0.4f" % (number, isEven - network.evaluate(number), network.evaluate(number))
