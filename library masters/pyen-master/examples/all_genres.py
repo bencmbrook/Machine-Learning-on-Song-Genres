c742d3fb804313c73e324ad07bc386ae439f0114
Benjamin Brook <benjaminbrook@college.harvard.edu>
2014-04-28 22:56:01 -0400

    a

--- library masters/pyen-master/examples/all_genres.py
+++
@@ -1,9 +0,0 @@
-'''
-    shows all genres
-'''
-import pyen
-
-en = pyen.Pyen()
-response = en.get('genre/list', bucket=['description'])
-for g in response['genres']:
-    print g['name'], '-', g['description']
