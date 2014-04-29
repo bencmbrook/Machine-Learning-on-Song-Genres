8449805fc90554d4b62e3dd1c3e1220095d21622
Benjamin Brook <benjaminbrook@college.harvard.edu>
2014-04-28 22:31:45 -0400

    Revert c067283..3781d37
    This rolls back to commit c0672833139c05797c0e8522b9c669d4c6162207.

--- code/node_edge.py
+++ code/node_edge.py
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-=======
-# file containing the ingredients for the network class in network.py:
-#   - Node
-#   - Edge
-#   - Input_Node and Output_Node subclasses 
-
->>>>>>> FETCH_HEAD
 class Node:
     def __init__(self):
         self.EdgesIn = []
@@ -17,23 +9,11 @@ class Node:
     def Evaluate(self, input):
         sum = 0
         
-<<<<<<< HEAD
         #sum the outputs of each incoming edge
         for e in self.EdgesIn
             sum += sum + (e.in.Evaluate(input) * e.weight)
          
         self.LastOutputs.append(sum)    
-=======
-        # store the outputs of each edge in an array
-        for i, e in enumerate(self.EdgesIn):
-            self.LastInput[i] = (e.in.Evaluate(input) * e.weight)
-        
-        # sum the elements of the input array
-        output = sum(self.LastInput)
-        
-        # store the value to be outputted in the output array
-        self.LastOutputs.append(output)    
->>>>>>> FETCH_HEAD
         return sum
 
     def evaluate(self, inputVector):
@@ -48,55 +28,16 @@ class Node:
       self.lastOutput = activationFunction(weightedSum)
       return self.lastOutput
     
-<<<<<<< HEAD
 
        
-=======
-    def EvalError(self, truth):
-        
-        # if for some reason we haven't learned from our last error
-        if self.Error != None
-            return self.Error
-        
-        # if current node is an output node calculate error and return it
-        # else if self.EdgesOut == []
-            # self.Error = truth - self.LastOutputs[0]
-            # return self.Error
-        
-        # else sum errors of output edges and eventually return error
-        else
-            for e in self.EdgesOut:
-                self.Error += (e.weight * e.out.EvalError(truth))
-                
-            return self.Error
-        
-    def Learn(self, LearnRate):
-        if not_(self.LastOutputs = [] or self.Error = None or self.LastInput = None)
-            for i, e in enumerate(self.EdgesIn):
-                e.weight += (LearnRate * self.LastOutputs[0] * (1 - self.LastOutputs[0]) *
-                             self.Error * self.LastInput[i]
->>>>>>> FETCH_HEAD
             
-        
-class Output_Node(Node):
-    def __init__(self, index)
-        Node.__init__(self, index)
-        
-        # add index of the output of this node for the creation of the single output vector
-        self.index = index
-        
-    def EvalError(self, truth)
-        self.Error = truth[self.index] - self.LastOutputs[0]
-        return self.Error
-        
         
 class Input_Node(Node): 
     def __init__(self, index):
         Node.__init__(self)
         
-        # add index of the input to be assigned to this node
+        #add index of the input to be assigned to this node
         self.index = index; 
-<<<<<<< HEAD
 
    
     def evaluate(self, inputVector):
@@ -106,22 +47,11 @@ class Input_Node(Node):
 
 class Edge
     def __init__(self, in, out):
-=======
-        
-    def Evaluate(self, input):
-        # should just return the correct value identified by index from the input
-        output = input[self.index]
-        self.LastOutputs.append(output)
-        return output
-
-class Edge:
-    def __init__(self, in, out)
->>>>>>> FETCH_HEAD
         self.weight = random.uniform(0,1)
         self.in = in
         self.out = out
         
-        # add self to input and output nodes
+        #add self to input and output nodes
         in.EdgesOut.append(self)
         out.EdgesIn.append(self)
 
--- library masters/pyen-master/examples/similar_artists.py
+++
@@ -1,8 +0,0 @@
-
-import pyen
-
-en = pyen.Pyen()
-
-response = en.get('artist/similar', name='weezer')
-for artist in response['artists']:
-    print artist['id'], artist['name']
